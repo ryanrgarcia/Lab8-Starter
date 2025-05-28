e81ff1f3ff4d557fb535df710dc2ea5e6183eeef
Ryan Garcia <178051155+rrgarcia19@users.noreply.github.com>
2025-05-27 21:30:43 -0700

    Implement Lab 8

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Lab8-Starter
+[Lab 8](https://ryanrgarcia.github.io/Lab8-Starter)
+No lab partners
+
+Graceful degredation and service workers are related in the sense that service workers are a means in which graceful degredation can be applied to web apps. Graceful degredation is the design principle and service workers are methods in which graceful degredation can be implemented. 
--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -33,27 +33,22 @@ async function init() {
  * of installing it and getting it running
  */
 function initializeServiceWorker() {
-  // EXPLORE - START (All explore numbers start with B)
-  /*******************/
-  // ServiceWorkers have many uses, the most common of which is to manage
-  // local caches, intercept network requests, and conditionally serve from
-  // those local caches. This increases performance since users aren't
-  // re-downloading the same resources every single page visit. This also allows
-  // websites to have some (if not all) functionality offline! I highly
-  // recommend reading up on ServiceWorkers on MDN before continuing.
-  /*******************/
-  // We first must register our ServiceWorker here before any of the code in
-  // sw.js is executed.
-  // B1. TODO - Check if 'serviceWorker' is supported in the current browser
-  // B2. TODO - Listen for the 'load' event on the window object.
-  // Steps B3-B6 will be *inside* the event listener's function created in B2
-  // B3. TODO - Register './sw.js' as a service worker (The MDN article
-  //            "Using Service Workers" will help you here)
-  // B4. TODO - Once the service worker has been successfully registered, console
-  //            log that it was successful.
-  // B5. TODO - In the event that the service worker registration fails, console
-  //            log that it has failed.
-  // STEPS B6 ONWARDS WILL BE IN /sw.js
+  // B1. Check if ServiceWorker is supported
+  if ('serviceWorker' in navigator) {
+    // B2. Listen for the 'load' event
+    window.addEventListener('load', function() {
+      // B3. Register the service worker
+      navigator.serviceWorker.register('./sw.js')
+        .then(function(registration) {
+          // B4. Log success
+          console.log('ServiceWorker registration successful with scope:', registration.scope);
+        })
+        .catch(function(error) {
+          // B5. Log failure
+          console.log('ServiceWorker registration failed:', error);
+        });
+    });
+  }
 }
 
 /**
@@ -65,41 +60,47 @@ function initializeServiceWorker() {
  * @returns {Array<Object>} An array of recipes found in localStorage
  */
 async function getRecipes() {
-  // EXPOSE - START (All expose numbers start with A)
-  // A1. TODO - Check local storage to see if there are any recipes.
-  //            If there are recipes, return them.
-  /**************************/
-  // The rest of this method will be concerned with requesting the recipes
-  // from the network
-  // A2. TODO - Create an empty array to hold the recipes that you will fetch
-  // A3. TODO - Return a new Promise. If you are unfamiliar with promises, MDN
-  //            has a great article on them. A promise takes one parameter - A
-  //            function (we call these callback functions). That function will
-  //            take two parameters - resolve, and reject. These are functions
-  //            you can call to either resolve the Promise or Reject it.
-  /**************************/
-  // A4-A11 will all be *inside* the callback function we passed to the Promise
-  // we're returning
-  /**************************/
-  // A4. TODO - Loop through each recipe in the RECIPE_URLS array constant
-  //            declared above
-  // A5. TODO - Since we are going to be dealing with asynchronous code, create
-  //            a try / catch block. A6-A9 will be in the try portion, A10-A11
-  //            will be in the catch portion.
-  // A6. TODO - For each URL in that array, fetch the URL - MDN also has a great
-  //            article on fetch(). NOTE: Fetches are ASYNCHRONOUS, meaning that
-  //            you must either use "await fetch(...)" or "fetch.then(...)". This
-  //            function is using the async keyword so we recommend "await"
-  // A7. TODO - For each fetch response, retrieve the JSON from it using .json().
-  //            NOTE: .json() is ALSO asynchronous, so you will need to use
-  //            "await" again
-  // A8. TODO - Add the new recipe to the recipes array
-  // A9. TODO - Check to see if you have finished retrieving all of the recipes,
-  //            if you have, then save the recipes to storage using the function
-  //            we have provided. Then, pass the recipes array to the Promise's
-  //            resolve() method.
-  // A10. TODO - Log any errors from catch using console.error
-  // A11. TODO - Pass any errors to the Promise's reject() function
+  // A1. Check local storage for recipes
+  const storedRecipes = localStorage.getItem('recipes');
+  if (storedRecipes) {
+    return JSON.parse(storedRecipes);
+  }
+  
+  // A2. Create empty array for recipes
+  const recipes = [];
+  
+  // A3. Return a new Promise
+  return new Promise((resolve, reject) => {
+    // A4. Loop through each recipe URL
+    let completedRequests = 0;
+    
+    RECIPE_URLS.forEach(async (url) => {
+      // A5. Try/catch block for async code
+      try {
+        // A6. Fetch the URL
+        const response = await fetch(url);
+        
+        // A7. Get JSON from the response
+        const recipeData = await response.json();
+        
+        // A8. Add recipe to array
+        recipes.push(recipeData);
+        
+        // A9. Check if all recipes are fetched
+        completedRequests++;
+        if (completedRequests === RECIPE_URLS.length) {
+          saveRecipesToStorage(recipes);
+          resolve(recipes);
+        }
+      } catch (error) {
+        // A10. Log error
+        console.error('Error fetching recipe:', error);
+        
+        // A11. Reject the promise with error
+        reject(error);
+      }
+    });
+  });
 }
 
 /**
